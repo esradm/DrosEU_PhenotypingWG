8c9664c445973b8241c578ad73809bce933db31a
vevel <envelkerdaffrec@gmail.com>
2022-04-03 21:45:08 +0200

    added meta plots

--- Code/meta_analyses.R
+++ Code/meta_analyses.R
@@ -15,6 +15,7 @@ library(lme4)
 library(lsmeans)
 library(afex)
 library(meta)
+library(ggpubr)
 
 
 ##### set working directory
@@ -26,22 +27,23 @@ source("Functions/lab_correlations_functions.R")
 source("Functions/meta_analysis_functions.R")
 
 ##### load data
-load("droseu_master_list.RData")
+droseu <- readRDS("Data/droseu_master_list.rds")
+estimates <- readRDS("LinearModelsPop/all_model_estimates.rds")
+
 
 ##### create output directory
 meta_dir <- "MetaAnalyses"
 dir.create(meta_dir, showWarnings = F) 
 
 
-
-
-
-estimates <- readRDS("LinearModelsPop/all_model_estimates.rds")
-
+##### define functions
 
 makeEffects <- function(x) {
   x <- mutate(x, Population = factor(Population, levels = c("YE","RE","GI","MU","MA","UM","KA","VA","AK")), Lab = as.factor(Lab), V = SE^2, Study = paste(Population, Lab, sep = "_"))
   x <- relocate(x, Trait, Population, Sex, Lab, Study) %>% arrange(Population) %>% dplyr::rename(Y = Estimate) }
+
+
+
 
 
 ############# VIABILITY #############
@@ -440,16 +442,107 @@ saveRDS(Pgm_meta, file = file.path(meta_dir, out_dir, "SR_F_meta.rds"))
 
 
 
+############# ouput all metas ############# 
+
+all_metas <- ls()[grep("_meta", ls())]
+saveRDS(all_metas, file = file.path(meta_dir, "all_metas.rds"))
 
 
 
-str(all_meta_analyses$Via_metagen , list.len = length(all_meta_analyses$Via_metagen))
 
-m$Q.b.random
 
-m$TE.random.w # summary effect
-m$seTE.random.w # summary effect SE
-m$lower.random.w #
-m$upper.random.w #
+
+######### PLOTS
+
+
+metas <- list.files(path = "MetaAnalyses", recursive = T, full.names = T, pattern = "meta.rds")
+
+
+for (i in 1:length(metas)){
+  f <- metas[i]
+  p_out_pdf <- sub(".rds", "_summary_effect.pdf", f)
+  p_out_png <- sub(".rds", "_summary_effect.png", f)
+  m <- readRDS(f)
+  m <- data.frame(Population = m$bylevs, Mstar = m$TE.random.w, SEMstar = m$seTE.random.w, Q = m$Q.b.random, p = m$pval.Q.b.random, LLMstar = m$lower.random.w, ULMstar = m$upper.random.w ) %>% mutate(Q_plot = paste0("italic(Q) == ", round(Q, 2)), p_plot = ifelse(p < 0.001, "italic(p) < 0.001", paste0("italic(p) == ", round(p, 4))))
+  
+  p_meta_SE <- ggplot(data = m, aes(x = Mstar, y = 1:length(Population), color = Population)) +
+    theme_bw() +
+    geom_point(size = 4, shape = 15) +
+    geom_errorbarh(aes(xmax = Mstar + SEMstar, xmin = Mstar - SEMstar), height = 0) +
+    scale_y_continuous(name = "Population", breaks = 1:length(m$Population), labels = m$Population) +
+    labs(x = "Summary effect", title = "Pop. summary effect with SE") +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + 
+    annotate("text", x = -Inf, y = Inf, label = unique(m$Q_plot), hjust=-0.2, vjust=3.2, parse = T) +
+    annotate("text", x = -Inf, y = Inf, label = unique(m$p_plot), hjust=-0.2, vjust=4.2, parse = T) +
+    theme(legend.position = "none")
+  
+  p_meta_CI <- ggplot(data = m, aes(x = Mstar, y = 1:length(Population), color = Population)) +
+    theme_bw() +
+    geom_point(size = 4, shape = 15) +
+    geom_errorbarh(aes(xmax = ULMstar, xmin = LLMstar), height = 0) +
+    scale_y_continuous(name = "Population", breaks = 1:length(m$Population), labels = m$Population) +
+    labs(x = "Summary effect", title = "Pop. summary effect with 95% CI") +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+    annotate("text", x = -Inf, y = Inf, label = unique(m$Q_plot), hjust=-0.2, vjust=3.2, parse = T) +
+    annotate("text", x = -Inf, y = Inf, label = unique(m$p_plot), hjust=-0.2, vjust=4.2, parse = T) +
+    theme(legend.position = "none")
+  
+  p_meta <- ggarrange(p_meta_SE, p_meta_CI)
+  
+  pdf(p_out_pdf, width = 8, height = 5)
+  print(p_meta)
+  dev.off()
+  
+  png(filename = p_out_png, height = 1500, width = 2400, res = 300)
+  print(p_meta)
+  dev.off()
+}
+  
+
+
+
+
+
+
+m <- Via_meta
+
+a <- data.frame(Population = m$bylevs, Mstar = m$TE.random.w, SEMstar = m$seTE.random.w, Q = m$Q.b.random, p = m$pval.Q.b.random, LLMstar = m$lower.random.w, ULMstar = m$upper.random.w ) %>% mutate(Q_plot = paste0("italic(Q) == ", round(Q, 2)), p_plot = ifelse(p < 0.001, "italic(p) < 0.001", paste0("italic(p) == ", round(p, 4))))
+
+p_meta_SE <- ggplot(data = a, aes(x = Mstar, y = 1:length(Population), color = Population)) +
+  theme_bw() +
+  geom_point(size = 4, shape = 15) +
+  geom_errorbarh(aes(xmax = Mstar + SEMstar, xmin = Mstar - SEMstar), height = 0) +
+  scale_y_continuous(name = "Population", breaks = 1:length(a$Population), labels = a$Population) +
+  labs(x = "Summary effect", title = "Pop. summary effect with SE") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + 
+  annotate("text", x = -Inf, y = Inf, label = unique(a$Q_plot), hjust=-0.2, vjust=3.5, parse = T) +
+  annotate("text", x = -Inf, y = Inf, label = unique(a$p_plot), hjust=-0.2, vjust=4.5, parse = T) +
+  theme(legend.position = "none")
+
+p_meta_CI <- ggplot(data = a, aes(x = Mstar, y = 1:length(Population), color = Population)) +
+  theme_bw() +
+  geom_point(size = 4, shape = 15) +
+  geom_errorbarh(aes(xmax = ULMstar, xmin = LLMstar), height = 0) +
+  scale_y_continuous(name = "Population", breaks = 1:length(a$Population), labels = a$Population) +
+  labs(x = "Summary effect", title = "Pop. summary effect with 95% CI") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  annotate("text", x = -Inf, y = Inf, label = unique(a$Q_plot), hjust=-0.2, vjust=3.5, parse = T) +
+  annotate("text", x = -Inf, y = Inf, label = unique(a$p_plot), hjust=-0.2, vjust=4.5, parse = T) +
+  theme(legend.position = "none")
+
+p_meta <- ggarrange(p_meta_SE, p_meta_CI)
+
+pdf(file="Viability/p_Via_meta.pdf", width=8, height=5)
+p_Via_meta
+dev.off()
+
+
+
+
+
+
+
+
+#str(m , list.len = length(m))
 
 
