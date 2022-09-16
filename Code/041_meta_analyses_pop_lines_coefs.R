323820c5153b14fde9ce2d2dc3eb4d46e277d636
vevel <envelkerdaffrec@gmail.com>
2022-09-16 10:32:14 +0200

    Updated H2

--- Code/041_meta_analyses_pop_lines_coefs.R
+++ Code/041_meta_analyses_pop_lines_coefs.R
@@ -143,12 +143,12 @@ write.csv(all_metas_line_wide, file = file.path(meta_dir, "all_models_line_meta_
 
 ##### quick plots for trait correlations
 
-#library(GGally)
-#corF <- ggpairs(select(compound_effects_wide, contains(c("_F", "NA"))))
-#ggsave(corF, file = "~/Desktop/trait_cor_F.pdf", width = 14, height = 14)
+library(GGally)
+corF <- ggpairs(select(all_metas_line_wide, contains(c("_F", "NA"))))
+ggsave(corF, file = "~/Desktop/trait_cor_F.pdf", width = 14, height = 14)
 
-#corM <- ggpairs(select(compound_effects_wide, contains(c("_M", "NA"))))
-#ggsave(corM, file = "~/Desktop/trait_cor_M.pdf", width = 14, height = 14)
+corM <- ggpairs(select(all_metas_line_wide, contains(c("_M", "NA"))))
+ggsave(corM, file = "~/Desktop/trait_cor_M.pdf", width = 14, height = 14)
 
 
 
--- Code/heritabilities.R
+++ Code/heritabilities.R
@@ -8,8 +8,19 @@
 
 
 ###### some things to consider
-# for between labs H2, instead of using mean Line values, one could consider using Line random effects extracted from individual labs linear mixed models
 
+
+
+
+# for LSP and CETS, H2 can be calculated for Population
+
+# what should be the input data when measures have been made at the individual level? Is each individual a line replicate or should we use the average value per replicate vial?
+
+# for between labs H2, how do we pool teh data from all labs together or do we first get an average line value per lab? Then each lab is treated as a replicate measure?
+
+# for between labs H2, instead of using mean Line values, one could consider using Line random coefficients extracted from individual labs linear mixed models
+
+# maybe just stick to within labs H2 in the first place?
 
 
 ##### clean workspace
@@ -23,26 +34,28 @@ library(lme4)
 setwd("~/Work/UNIFR/GitHub/DrosEU_PhenotypingWG/")
 
 
-##### load data
-droseu <- readRDS("Data/droseu_master_list_2022-05-02.rds")
-line_re <- read.csv("LinearModelsPop/all_models_line_random_effects.csv")
 
-##### functions
+################# DEFINE H2 FUNCTION ################# 
 
-# this function, based on a linear mixed model (lmer) in which genotypes (i.e. lines) are included as random effect variables, allows to extract genetic and environmental variances to compute broad sense heritability, H2.
-# usage: H2_lmm(data = via_df, phenot = "via", genot = "Line")
-# data: data.frame with columns for trait values and genotypes
+# this function is based on a linear mixed model (lmer) in which genotypes (i.e. lines) are included as random-effect variables. It allows for the extraction of genetic and environmental variances used to compute broad sense heritability, H2.
+# usage: H2_lmm(d = via_df, phenot = "via", genot = "Line")
+# d: data.frame with columns for trait values and genotypes
 # phenot: trait values
 # genot: genotype ids
+# min_rep_genot: minimal genotype replication allowed
 
-H2_lmm <- function(data, phenot, genot) {
+H2_lmm <- function(d, phenot, genot, min_rep_genot = 1) {
   require("lme4")
-  # make sure that H can be computed by checking that genotypes have been measured more than once. If not the case H cannot be computed and NA is returned.
-  if(length(unlist(unique(data[,genot]))) == nrow(data)){ 
-    H2 <- "NA"
+  # check genotypes
+  n_unique_genot <- length(unlist(unique(d[,genot])))
+  d <- d %>% group_by(.data[[genot]]) %>% filter(n() >= min_rep_genot)
+  n_rep_genot <- length(unlist(unique(d[,genot])))
+  # make sure that H can be computed by checking that genotypes have been measured more than once (i.e. that genotypes are replicated). If not the case H cannot be computed and a data.frame with NAs is returned.
+  if (n_rep_genot < 2 | n_unique_genot == nrow(d)) { 
+    data.frame(H2 = NA, Vp = NA, Vg = NA, Ve = NA, Genot = genot, N_unique_genot = n_unique_genot, Min_rep_genot = min_rep_genot, N_rep_genot = n_rep_genot, Warnings = "Not enough genotype replicates")
   } else {
-    # build and run the lmer model with genot as random effect variable
-    lmm <- lmer(as.formula(paste0(phenot, "~1 + 1|", genot)), data = data)	
+    # build and run the lmer model with genot as random-effect variable
+    lmm <- lmer(as.formula(paste0(phenot, "~1 + 1|", genot)), data = d)	
     # extract variance components
     lmm.varcor <- as.data.frame(summary(lmm)$varcor)
     # variance explained by genot
@@ -52,57 +65,125 @@ H2_lmm <- function(data, phenot, genot) {
     # total variance
     Vp <- Vg + Ve
     # calculate H2
-    H2 <- Vg/Vp; H2
+    H2 <- round(Vg/Vp, 2)
+    # output
+    w <- summary(lmm)$optinfo$conv$lme4$messages
+    data.frame(H2, Vp, Vg, Ve, Genot = genot, N_unique_genot = n_unique_genot, Min_rep_genot = min_rep_genot, N_rep_genot = n_rep_genot, Warnings = ifelse(length(w) != 0, w, "NA"))
   }
 }
 
 
-##### example with viability
-
-### calculate within lab H2 using Line values. This works only for labs that have several measurements per line (all labs except Schmidt)
-
-# split to a list by PI
-via_df_pi <- split(droseu$via, f = droseu$via$Supervisor.PI)
-
-# apply H2_lmm to each element of the list
-h2_via_pi <- unlist(lapply(via_df_pi, H2_lmm, "ProportionEggtoAdultSurvival", "Line"))
-h2_via_pi <- data.frame(Lab = names(h2_via_pi), H2 = h2_via_pi)
 
 
-### calculate between labs H2 with per lab mean Line values
+################# LINE H2 WITHIN LAB WITH RAW DATA #################
 
-# aggregate line vials to get mean line values for each lab
-via_df_line <- group_by(droseu$via, Supervisor.PI, Line) %>%
-  summarise(via = mean(ProportionEggtoAdultSurvival), n = n())
+# code below allows for automated calculation of H2 for all traits
 
-# apply H2_lmm
-h2_via <- H2_lmm(via_df_line, "via", "Line")
+# get data in
+trait_names <- read.csv("InfoTables/trait_names.csv")
+droseu <- readRDS("Data/droseu_master_list_2022-05-02.rds")
+
+# add dummy Sex and Line columns if not present for easier batch processing
+for (i in 1:length(droseu)) {
+  if (!"Sex" %in% colnames(droseu[[i]])) droseu[[i]]$Sex <- "NA"
+  if (!"Line" %in% colnames(droseu[[i]])) droseu[[i]]$Line <- "NA"
+}
+
+# keep variables of interest and make long table
+for (i in 1:length(droseu)) {
+  droseu[[i]] <- dplyr::select(droseu[[i]], contains(c("Supervisor.PI", "Population", "Line", "Sex", trait_names$Trait_name_raw)))
+  droseu[[i]] <- dplyr::select(droseu[[i]], -contains("Population_"))
+  droseu[[i]] <- pivot_longer(droseu[[i]], cols = contains(trait_names$Trait_name_raw), names_to = "Trait_name_raw", values_to = "Value")
+}
+
+# update trait names
+droseu <- bind_rows(droseu) %>% 
+  inner_join(trait_names, by = "Trait_name_raw") %>% 
+  dplyr::select(-c("Trait_handle", "Trait_name_raw")) %>%
+  rename(Lab = Supervisor.PI)
+
+# keep traits that can be used for Line H2
+droseu_line <- filter(droseu, !Trait %in% c("LSM", "LSP", "CETS"))
+
+# split to list
+droseu_line_list <- group_split(droseu_line, Trait, Sex, Lab)
+
+# loop over traits to calculate H2
+h2_line_within <- list()
+for (i in 1:length(droseu_line_list)) {
+  trait <- droseu_line_list[[i]][1, c("Trait", "Sex", "Lab")]
+  h2_lmm <- H2_lmm(droseu_line_list[[i]] %>% filter(!Value %in% c(-Inf, Inf)), "Value", "Line", 1)
+  h2_line_within[[i]] <- bind_cols(trait, h2_lmm) 
+}
+h2_line_within <- bind_rows(h2_line_within)
+  
+# save results
+write.csv(h2_line_within, "Heritability/H2_line_raw_data_within_labs.csv", row.names = F)
 
 
-### calculate between labs H2 with Line random effects
-h2_via_re <- H2_lmm(filter(line_re, Trait == "Via"), "Estimate", "Line")
+
+
+################# LINE H2 BETWEEN LABS WITH RAW DATA #################
+
+
+# split to list
+droseu_line_list2 <- group_split(droseu_line, Trait, Sex)
+
+# loop over traits to calculate H2
+h2_line_between <- list()
+for (i in 1:length(droseu_line_list2)) {
+  trait <- droseu_line_list2[[i]][1, c("Trait", "Sex")]
+  h2_lmm <- H2_lmm(droseu_line_list2[[i]] %>% filter(!Value %in% c(-Inf, Inf)), "Value", "Line", 1)
+  h2_line_between[[i]] <- bind_cols(trait, h2_lmm) 
+}
+h2_line_between <- bind_rows(h2_line_between)
+
+# save results
+write.csv(h2_line_between, "Heritability/H2_line_raw_data_between_labs.csv", row.names = F)
 
 
 
 
 
 
-##### deprecated
-
-#wa <- group_by(droseu$wa, Supervisor.PI, Line, ReplicateVial) %>%
-#  summarise(WAL = mean(CentroidSizeLeft_micrometers), WAR = mean(CentroidSizeRight_micrometers), n = n())
-
-#wa_line <- group_by(wa, Supervisor.PI, Line)%>%
-#  summarise(WAL = mean(WAL), WAR = mean(WAR), n = n())
-
-#h2_wal <- H2_lmm(wa_line, "WAL", "Line")
-#h2_war <- H2_lmm(wa_line, "WAR", "Line")
-
-#wa_pi <- split(wa, f = wa$Supervisor.PI)
-
-#h2_wal_pi <- lapply(wa_pi, H2_lmm, "WAL", "Line")
-#h2_war_pi <- lapply(wa_pi, H2_lmm, "WAR", "Line")
 
 
-#lmer(DW_micrograms ~ Population + (1|Line), data = droseu$dw)
+
+
+
+
+
+################# LINE H2 BETWEEN LABS WITH LMERS RANDOM COEFS #################
+
+# get data in
+lrc <- read.csv("LinearModelsPop/all_models_line_random_coefs.csv")
+
+# split to list
+lrc_list <- group_split(lrc, Trait, Sex)
+
+# calculate H2
+trait <- bind_rows(lapply(lrc_list, function(x) x[1,c("Trait", "Sex")]))
+h2_lmm <- bind_rows(lapply(lrc_list, H2_lmm, "Coef", "Line", 1))
+h2_line_between_rc <- bind_cols(trait, h2_lmm)
+
+# save results
+write.csv(h2_line_between_rc, "Heritability/H2_line_random_coefs_between_labs.csv", row.names = F)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
