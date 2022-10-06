a4c14630994d6c2e6e103577bb6ae9aa7dda76fd
vevel <envelkerdaffrec@gmail.com>
2022-10-06 12:19:54 +0200

    added png plot

--- Code/trait_correlations.r
+++ Code/trait_correlations.r
@@ -25,22 +25,22 @@ rc_wide <- rc %>%
 
 cor_f9 <- ggpairs(select(rc_wide, matches(c9) & matches("_F")))
 ggsave(cor_f9,
-    file = "TraitCorrelations/trait_correlations_f9_line_random_coefs.pdf",
+    file = "TraitCorrelations/trait_correlations_f9_line_random_coefs.png",
     width = 10, height = 10)
 
 cor_m9 <- ggpairs(select(rc_wide, matches(c9) & matches("_M")))
 ggsave(cor_m9,
-    file = "TraitCorrelations/trait_correlations_m9_line_random_coefs.pdf",
+    file = "TraitCorrelations/trait_correlations_m9_line_random_coefs.png",
     width = 10, height = 10)
 
 cor_fmax <- ggpairs(select(rc_wide, matches(fmax) & matches("_F")))
 ggsave(cor_fmax,
-    file = "TraitCorrelations/trait_correlations_fmax_line_random_coefs.pdf",
+    file = "TraitCorrelations/trait_correlations_fmax_line_random_coefs.png",
     width = 10, height = 10)
 
 cor_fmaxp <- ggpairs(select(rc_wide, matches(fmaxp) & !matches("_M")))
 ggsave(cor_fmaxp,
-    file = "TraitCorrelations/trait_correlations_fmaxp_line_random_coefs.pdf",
+    file = "TraitCorrelations/trait_correlations_fmaxp_line_random_coefs.png",
     width = 10, height = 10)
 
 
@@ -51,20 +51,20 @@ rc_diet_wide <- rc_diet %>%
 
 cor_f9_diet <- ggpairs(select(rc_diet_wide, matches(c9) & matches("_F")))
 ggsave(cor_f9_diet,
-    file = "TraitCorrelations/trait_correlations_f9_line_random_coefs_diet.pdf",
+    file = "TraitCorrelations/trait_correlations_f9_line_random_coefs_diet.png",
     width = 10, height = 10)
 
 cor_m9_diet <- ggpairs(select(rc_diet_wide, matches(c9) & matches("_M")))
 ggsave(cor_m9_diet,
-    file = "TraitCorrelations/trait_correlations_m9_line_random_coefs_diet.pdf",
+    file = "TraitCorrelations/trait_correlations_m9_line_random_coefs_diet.png",
     width = 10, height = 10)
 
 cor_fmax_diet <- ggpairs(select(rc_diet_wide, matches(fmax) & matches("_F")))
 ggsave(cor_fmax_diet,
-    file = "TraitCorrelations/trait_correlations_fmax_line_random_coefs_diet.pdf",
+    file = "TraitCorrelations/trait_correlations_fmax_line_random_coefs_diet.png",
     width = 10, height = 10)
 
 cor_fmaxp_diet <- ggpairs(select(rc_diet_wide, matches(fmaxp) & !matches("_M")))
 ggsave(cor_fmaxp_diet,
-    file = "TraitCorrelations/trait_correlations_fmaxp_line_random_coefs_diet.pdf",
+    file = "TraitCorrelations/trait_correlations_fmaxp_line_random_coefs_diet.png",
     width = 10, height = 10)
