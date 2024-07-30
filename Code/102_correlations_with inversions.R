3dde714656042d4c5c269a2ec9062cf532bcc64c
vevel <envelkerdaffrec@gmail.com>
2024-07-30 23:05:29 +0200

    Inversion analyses

--- Code/102_correlations_with inversions.R
+++ Code/102_correlations_with inversions.R
@@ -7,6 +7,8 @@ library(tidyverse)
 library(ggrepel)
 library(ggpubr)
 library(MetBrewer)
+library(lme4)
+library(afex)
 
 
 ##### set working directory
@@ -319,3 +321,259 @@ ggsave(
   filename = "Inversions/pop_3Rmo_pearson_correlations_facets.png",
   height = 10, width = 14, dpi = 120
 )
+
+
+
+
+
+# line level GLMs
+
+
+## four lines "RE9"  "UM22" "UM24" "AK19" have not been typed for inversions
+
+line_invs <- inner_join(
+  line_comp,
+  invs %>% select(contains("In")) %>% rename(Line = line_id)
+) %>%
+  mutate(In2Lt = ifelse(In2Lt == 0.5 | In2Lt == 1, 1, 0)) %>%
+  group_split(Trait, Sex)
+
+
+# In3RP
+
+line_3rp_lm <- list()
+for (i in 1:length(line_invs)) {
+  inv_lm <- lm(Estimate ~ as.factor(In3RP), data = line_invs[[i]])
+  line_3rp_lm[[i]] <- data.frame(
+    Trait = unique(line_invs[[i]]$Trait),
+    Sex = unique(line_invs[[i]]$Sex),
+    R = summary(inv_lm)$r.squared,
+    P = summary(inv_lm)$coefficients[2,4],
+    P_bonf = summary(inv_lm)$coefficients[2,4] * length(line_invs)
+  )
+}
+
+line_3rp_lm <- bind_rows(line_3rp_lm) %>%
+  mutate(
+    P_bonf = ifelse(P_bonf > 1, 1, P_bonf),
+    Sex = factor(Sex, levels = c("F", "M", "B", "NA")),
+    Label = paste(Trait, Sex, sep = "_")
+  )
+
+
+write.csv(line_3rp_lm, "Inversions/line_3RP_lms.csv", row.names = F)
+
+
+
+line_3rp_lm_facet <- bind_rows(line_invs) %>%
+  mutate(Label = paste(Trait, Sex, sep = "_")) %>%
+  ggplot(aes(x = as.factor(In3RP), y = Estimate, color = Population)) +
+  geom_quasirandom(size = 1, alpha = 0.5, width = .2) +
+  colScale +
+  facet_wrap(Label ~., scales = "free", ncol = 7) +
+  labs(
+    title = "Effect of In(3R)P on measured traits - Line level",
+    x = "In(3R)P",
+    y = "Line estimates"
+  ) +
+  theme_bw(14)
+
+ggsave(
+  line_3rp_lm_facet,
+  filename = "Inversions/line_3RP_lm_facets.png",
+  height = 10, width = 14, dpi = 120
+)
+
+
+
+
+
+
+# In3Rmo
+
+line_3rmo_lm <- list()
+for (i in 1:length(line_invs)) {
+  inv_lm <- lm(Estimate ~ as.factor(In3Rmo), data = line_invs[[i]])
+  line_3rmo_lm[[i]] <- data.frame(
+    Trait = unique(line_invs[[i]]$Trait),
+    Sex = unique(line_invs[[i]]$Sex),
+    R = summary(inv_lm)$r.squared,
+    P = summary(inv_lm)$coefficients[2,4],
+    P_bonf = summary(inv_lm)$coefficients[2,4] * length(line_invs)
+  )
+}
+
+line_3rmo_lm <- bind_rows(line_3rmo_lm) %>%
+  mutate(
+    P_bonf = ifelse(P_bonf > 1, 1, P_bonf),
+    Sex = factor(Sex, levels = c("F", "M", "B", "NA")),
+    Label = paste(Trait, Sex, sep = "_")
+  )
+
+write.csv(line_3rmo_lm, "Inversions/line_3Rmo_lms.csv", row.names = F)
+
+
+line_3rmo_lm_facet <- bind_rows(line_invs) %>%
+  mutate(Label = paste(Trait, Sex, sep = "_")) %>%
+  ggplot(aes(x = as.factor(In3Rmo), y = Estimate, color = Population)) +
+  geom_quasirandom(size = 1, alpha = 0.5, width = .2) +
+  colScale +
+  facet_wrap(Label ~., scales = "free", ncol = 7) +
+  labs(
+    title = "Effect of In(3R)mo on measured traits - Line level",
+    x = "In(3R)mo",
+    y = "Line estimates"
+  ) +
+  theme_bw(14)
+
+ggsave(
+  line_3rmo_lm_facet,
+  filename = "Inversions/line_3Rmo_lm_facets.png",
+  height = 10, width = 14, dpi = 120
+)
+
+
+
+# In2Lt
+
+line_2Lt_lm <- list()
+for (i in 1:length(line_invs)) {
+  inv_lm <- lm(Estimate ~ as.factor(In2Lt), data = line_invs[[i]])
+  line_2Lt_lm[[i]] <- data.frame(
+    Trait = unique(line_invs[[i]]$Trait),
+    Sex = unique(line_invs[[i]]$Sex),
+    R = summary(inv_lm)$r.squared,
+    P = summary(inv_lm)$coefficients[2,4],
+    P_bonf = summary(inv_lm)$coefficients[2,4] * length(line_invs)
+  )
+}
+
+line_2Lt_lm <- bind_rows(line_2Lt_lm) %>%
+  mutate(
+    P_bonf = ifelse(P_bonf > 1, 1, P_bonf),
+    Sex = factor(Sex, levels = c("F", "M", "B", "NA")),
+    Label = paste(Trait, Sex, sep = "_")
+  )
+
+
+write.csv(line_2Lt_lm, "Inversions/line_2Lt_lms.csv", row.names = F)
+
+
+
+line_2Lt_lm_facet <- bind_rows(line_invs) %>%
+  mutate(Label = paste(Trait, Sex, sep = "_")) %>%
+  ggplot(aes(x = as.factor(In2Lt), y = Estimate, color = Population)) +
+  geom_quasirandom(size = 1, alpha = 0.5, width = .2) +
+  colScale +
+  facet_wrap(Label ~., scales = "free", ncol = 7) +
+  labs(
+    title = "Effect of In(2L)t on measured traits - Line level",
+    x = "In(2L)t",
+    y = "Line estimates"
+  ) +
+  theme_bw(14)
+
+ggsave(
+  line_2Lt_lm_facet,
+  filename = "Inversions/line_2Lt_lm_facets.png",
+  height = 10, width = 14, dpi = 120
+)
+
+
+
+
+
+# In3LP
+
+line_3lp_lm <- list()
+for (i in 1:length(line_invs)) {
+  inv_lm <- lm(Estimate ~ as.factor(In3LP), data = line_invs[[i]])
+  line_3lp_lm[[i]] <- data.frame(
+    Trait = unique(line_invs[[i]]$Trait),
+    Sex = unique(line_invs[[i]]$Sex),
+    R = summary(inv_lm)$r.squared,
+    P = summary(inv_lm)$coefficients[2,4],
+    P_bonf = summary(inv_lm)$coefficients[2,4] * length(line_invs)
+  )
+}
+
+line_3lp_lm <- bind_rows(line_3lp_lm) %>%
+  mutate(
+    P_bonf = ifelse(P_bonf > 1, 1, P_bonf),
+    Sex = factor(Sex, levels = c("F", "M", "B", "NA")),
+    Label = paste(Trait, Sex, sep = "_")
+  )
+
+
+write.csv(line_3lp_lm, "Inversions/line_3LP_lms.csv", row.names = F)
+
+
+
+line_3lp_lm_facet <- bind_rows(line_invs) %>%
+  mutate(Label = paste(Trait, Sex, sep = "_")) %>%
+  ggplot(aes(x = as.factor(In3LP), y = Estimate, color = Population)) +
+  geom_quasirandom(size = 1, alpha = 0.5, width = .2) +
+  colScale +
+  facet_wrap(Label ~., scales = "free", ncol = 7) +
+  labs(
+    title = "Effect of In(3L)P on measured traits - Line level",
+    x = "In(3L)P",
+    y = "Line estimates"
+  ) +
+  theme_bw(14)
+
+ggsave(
+  line_3lp_lm_facet,
+  filename = "Inversions/line_3LP_lm_facets.png",
+  height = 10, width = 14, dpi = 120
+)
+
+
+
+
+# In2Rns
+
+line_2rns_lm <- list()
+for (i in 1:length(line_invs)) {
+  inv_lm <- lm(Estimate ~ as.factor(In2RNs), data = line_invs[[i]])
+  line_2rns_lm[[i]] <- data.frame(
+    Trait = unique(line_invs[[i]]$Trait),
+    Sex = unique(line_invs[[i]]$Sex),
+    R = summary(inv_lm)$r.squared,
+    P = summary(inv_lm)$coefficients[2,4],
+    P_bonf = summary(inv_lm)$coefficients[2,4] * length(line_invs)
+  )
+}
+
+line_2rns_lm <- bind_rows(line_2rns_lm) %>%
+  mutate(
+    P_bonf = ifelse(P_bonf > 1, 1, P_bonf),
+    Sex = factor(Sex, levels = c("F", "M", "B", "NA")),
+    Label = paste(Trait, Sex, sep = "_")
+  )
+
+
+write.csv(line_2rns_lm, "Inversions/line_2RNs_lms.csv", row.names = F)
+
+
+
+line_2rns_lm_facet <- bind_rows(line_invs) %>%
+  mutate(Label = paste(Trait, Sex, sep = "_")) %>%
+  ggplot(aes(x = as.factor(In2RNs), y = Estimate, color = Population)) +
+  geom_quasirandom(size = 1, alpha = 0.5, width = .2) +
+  colScale +
+  facet_wrap(Label ~., scales = "free", ncol = 7) +
+  labs(
+    title = "Effect of In(2R)Ns on measured traits - Line level",
+    x = "In(2R)Ns",
+    y = "Line estimates"
+  ) +
+  theme_bw(14)
+
+ggsave(
+  line_2rns_lm_facet,
+  filename = "Inversions/line_2RNs_lm_facets.png",
+  height = 10, width = 14, dpi = 120
+)
+
+
